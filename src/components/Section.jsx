514776df356e4dd172e50435115c7ec1ec7e5b53
Zoha Afzal <160478899+Zoha-Afzal@users.noreply.github.com>
2025-04-20 13:28:58 +0500

    Section.jsx

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -5,11 +5,12 @@ const Section = ({
   id,
   crosses,
   crossesOffset,
-  customPadding,
+  customPaddings,
   children,
 }) => {
   return (
     <div
+      id={id}
       className={`relative ${
         customPaddings ||
         `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
@@ -18,14 +19,16 @@ const Section = ({
       {children}
 
       <div className="pointer-events-none absolute left-5 top-0 hidden h-full w-0.25 bg-stroke-1 md:block lg:left-7.5 xl:left-10" />
-
       <div className="pointer-events-none absolute right-5 top-0 hidden h-full w-0.25 bg-stroke-1 md:block lg:right-7.5 xl:right-10" />
+
       {crosses && (
         <>
           <div
-            className={`absoute $ left-7.5 right-7.5 top-0 hidden h-0.25 bg-stroke-1 ${crossesOffset && crossesOffset} pointer-events-none right-10 lg:block xl:left-10`}
+            className={`absolute left-7.5 right-7.5 top-0 hidden h-0.25 bg-stroke-1 ${
+              crossesOffset && crossesOffset
+            } pointer-events-none right-10 lg:block xl:left-10`}
           />
-          <SectionSvg crossesOffset={}/>
+          <SectionSvg crossesOffset={crossesOffset} />
         </>
       )}
     </div>
